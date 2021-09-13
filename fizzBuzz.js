2340d649647dec85084e188c435f3742c86416df
Oksana Dubik <oksana170773@gmail.com>
2021-09-13 19:41:33 +0600

    bugs fixed

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,15 +1,17 @@
-let a = [1];
-let b = 1;
-console.log(b)
-for (let i = 1; i < 100; i++) {
-    let c = a.push(0);
-    if (c % 3 === 0 && c % 5 !== 0) {
-        console.log("Fizz");
-    } else if (c % 5 === 0 && c % 3 !== 0) {
-        console.log("Buzz");
-    } else if (c % 5 === 0 || c % 3 === 0) {
-        console.log("FizzBuzz");
-    } else {
-        console.log(c);
+function outputNumbers(amountNumbers) {
+    let arr = [1];
+    for (let i = 1; i <= amountNumbers; i++) {
+        let increasingArr = arr++;
+        if (increasingArr % 3 === 0 && increasingArr % 5 !== 0) {
+            console.log("Fizz");
+        } else if (increasingArr % 5 === 0 && increasingArr % 3 !== 0) {
+            console.log("Buzz");
+        } else if (increasingArr % 5 === 0 || increasingArr % 3 === 0) {
+            console.log("FizzBuzz");
+        } else {
+            console.log(increasingArr);
+        }
     }
 }
+
+outputNumbers(100);
