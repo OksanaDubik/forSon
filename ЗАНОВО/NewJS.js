9af5a32106448176a9777ce4f971e12baea0c972
Oksana Dubik <oksana170773@gmail.com>
2021-10-17 22:46:35 +0600

    не знаю как сделать корзинку убрала все, что касается Vue

--- ЗАНОВО/NewJS.js
+++ ЗАНОВО/NewJS.js
@@ -1,56 +1,5 @@
 const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/'
 
-const app = new Vue({
-    el: "#app",
-    data: {
-        goods:[],
-        filtred:[],
-        searchLine: ""
-    },
-    methods: {
-        makeGETRequest(url, callback) {
-            const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
-
-        }
-    }
-})
-
-
-
-
-// function send(onError, onSuccess, url, method = 'GET', data = null, headers = [], timeout = 60000) {
-//     let xhr;
-//
-//     if (window.XMLHttpRequest) {
-//         // Chrome, Mozilla, Opera, Safari
-//         xhr = new XMLHttpRequest();
-//     }  else if (window.ActiveXObject) {
-//         // Internet Explorer
-//         xhr = new ActiveXObject("Microsoft.XMLHTTP");
-//     }
-//
-//     xhr.open(method, url, true);
-//
-//
-//     headers.forEach((header) => {
-//         xhr.setRequestHeader(header.key, header.value);
-//     })
-//
-//
-//     xhr.timeout = timeout;
-//
-//     xhr.onreadystatechange = function () {
-//         if (xhr.readyState === 4) {
-//             if(xhr.status >= 400) {
-//                 onError(xhr.statusText)
-//             } else {
-//                 onSuccess(xhr.responseText)
-//             }
-//         }
-//     }
-//
-//     xhr.send(data);
-// }
 const searchInput = document.querySelector(".goods-search");
 const searchBtn = document.querySelector(".search-button")
 
