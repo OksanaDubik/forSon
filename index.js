67895be950d9d83a9903242cc24f9e32b3d423b2
Oksana Dubik <oksana170773@gmail.com>
2022-04-30 16:05:56 +0600

    новый реакт удалила чужой код

--- index.js
+++
@@ -1,7 +0,0 @@
-const getData = async (url)=>{
-    const res= await fetch(url)
-    const json = await res.json()
-    return json
-}
-const url = "https://jsonplaceholder.typicode.com/todos"
-const data = await getData(url)
